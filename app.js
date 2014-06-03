f3534562471ecf8addfc3d69fd2179b4538831ef
Marta Buda <bmarta1991@o2.pl>
2014-06-03 20:17:50 +0200

    chat works

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-var express = require('express');
-var ECT = require('ect');
 var http = require('http');
+var express = require('express');
+var app = express();
+var httpServer = http.createServer(app);
+var ECT = require('ect');
+var ectRenderer = ECT({ watch: true, root: __dirname + '/views', ext : '.ect' });
 var path = require('path');
 var less = require('less-middleware');
-var ectRenderer = ECT({ watch: true, root: __dirname + '/views', ext : '.ect' });
-
-var app = express();
+var socket = require('socket.io');
+var io = socket.listen(httpServer);
+var _ = require("underscore");
+var redis = require('redis');
+var redisClient = redis.createClient();
+var sess = require('express-session');
+var RedisStore = require('connect-redis')(sess);
+var rClient = redis.createClient();
+var sessionStore = new RedisStore({client:rClient});
+var cookieParser = express.cookieParser('S3cr3t');
 
 app.configure(function() {
   app.set('port', process.env.PORT || 3000);
@@ -14,9 +24,10 @@ app.configure(function() {
   app.engine('ect', ectRenderer.render);
   app.use(express.favicon());
   app.use(express.logger('dev'));
-  app.use(express.json());
-  app.use(express.urlencoded());
+  app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(cookieParser);
+  app.use(express.session({store:sessionStore, key:'jsessionid', secret:'S3cr3t'}));
   app.use(less(path.join(__dirname, 'src', 'less'), {
     dest: path.join(__dirname, 'public'),
     preprocess: {
@@ -44,6 +55,65 @@ app.get('/about', function (req, res){
   res.render('about');
 });
 
-http.createServer(app).listen(3000, function() {
-  console.log('Server on port: ' + app.get('port'));
+app.get('/chat', function (req, res){
+  var user = req.session.user;
+  req.session.regenerate(function (err) {
+    req.session.user = user;
+    res.render('chat', {user:req.session.user});
+  });
+});
+
+app.post('/user', function(req,res) {
+  req.session.user = req.body.user;
+  res.json({"error": ""});
+});
+
+var SessionSockets = require('session.socket.io');
+var sessionSockets = new SessionSockets(io, sessionStore, cookieParser, 'jsessionid');
+
+var numChatters = 0;
+var chatters = [];
+var sub = redis.createClient();
+var pub = redis.createClient();
+sub.subscribe('chat');
+
+sessionSockets.on('connection', function (err, socket, session) {
+    if(!session.user) return;
+
+    socket.on('chat', function (data) {
+      var msg = JSON.parse(data);
+      var reply = JSON.stringify({action:'message', user:session.user, msg:msg.msg });
+        pub.publish('chat', reply);
+    });
+
+    socket.on('join', function () {
+      var reply = JSON.stringify({action :'control', user:session.user, msg:' joined the channel' });
+      pub.publish('chat', reply);
+      ++numChatters;
+      io.sockets.emit('update numChatters', numChatters);
+      io.sockets.emit('add chatter', session.user);
+      redisClient.smembers('chatters', function(err, names) {
+        names.forEach(function(name) {
+          socket.emit('add chatter', name);
+        });
+      redisClient.sadd("chatters", session.user);
+      });
+    });
+    sub.on('message', function (channel, message) {
+        socket.emit(channel, message);
+    });
+
+    socket.on('disconnect', function() {
+      var reply = JSON.stringify({action:'control', user:session.user, msg:' leaved the channel' });
+      pub.publish('chat', reply);
+      --numChatters;
+      io.sockets.emit('update numChatters', numChatters);
+      io.sockets.emit('remove chatter', session.user);
+      redisClient.srem("chatters", session.user);
+    });
+});
+var port = app.get('port');
+
+httpServer.listen(port, function() {
+  console.log("Server is listening on port: " + port);
 });
