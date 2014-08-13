d0b257636c28ce9aede99f8a49184306f886d55d
Marta Buda <bmarta1991@o2.pl>
2014-08-13 19:24:14 +0200

    add jquery on hover animation in menu

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,26 @@
 /*jshint globalstrict: true, devel: true, browser: true */
-console.log('Benkyou Helper!');
+console.log('Benkyou Helper welcomes you!');
+
+$(document).ready(function () {
+  $('.nav li').hover(function(){
+    if(!$(this).hasClass('animated')) {
+      $(this).dequeue().stop().animate({bottom: '10px', right: '7px'});
+    }
+  }, function() {
+    $(this).addClass('animated').animate({bottom: '0px', right: '0px'}, 'normal', 'linear', function() {
+      $(this).removeClass('animated').dequeue();
+    });
+  });
+
+  /* I want these hearts to beat on hover but do not now how to repeat it
+  $('.footer').hover(function() {
+    if(!$('.glyphicon-heart').hasClass('animated')) {
+      $('.glyphicon-heart').dequeue().stop().animate({fontSize: '18px', top: '4px'});
+    }
+  }, function() {
+    $('.glyphicon-heart').addClass('animated').animate({fontSize: '14px', top: '0'}, 'normal', 'linear', function() {
+      $('.glyphicon-heart').removeClass('animated').dequeue();
+    });
+  });
+  */
+});
